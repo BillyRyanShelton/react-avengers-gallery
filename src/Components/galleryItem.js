1c90efef23018839dad87aaf752a62c0fc50476e
Billy Shelton <sheltonbilly@utexas.edu>
2019-03-15 23:56:27 -0500

    added a gallery, gallery item, header and nav components.

---
+++ src/Components/galleryItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const gallery-item = props => (
+
+);
+
+export default gallery-item;
---
+++ src/Components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = (props) => {
+//app title
+//logo
+//nav
+//search bar
+};
+
+
+export default Header;
