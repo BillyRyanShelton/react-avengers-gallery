5b6849bed9719f9dcade5e611b61fe0fbe32efef
Billy Shelton <sheltonbilly@utexas.edu>
2019-03-20 20:31:13 -0500

    Updates

---
+++ precache-manifest.49079b486a1bd9fa0a6a257f17bee46f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc75a56eec42f2f3c6ea",
+    "url": "/react-avengers-gallery/static/css/main.edce91df.chunk.css"
+  },
+  {
+    "revision": "fc75a56eec42f2f3c6ea",
+    "url": "/react-avengers-gallery/static/js/main.4f5fdc9d.chunk.js"
+  },
+  {
+    "revision": "e9cdd16e852b73087077",
+    "url": "/react-avengers-gallery/static/js/runtime~main.4e95fb55.js"
+  },
+  {
+    "revision": "a4220da4a75c3345bd38",
+    "url": "/react-avengers-gallery/static/js/2.705836fd.chunk.js"
+  },
+  {
+    "revision": "dd1d845217eb7dc0d1f42e2b6f8737f1",
+    "url": "/react-avengers-gallery/index.html"
+  }
+];
