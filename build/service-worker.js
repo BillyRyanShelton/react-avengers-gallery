ab9bd55cbb69aa8f7b654d100eb4798f080532de
Billy Shelton <sheltonbilly@utexas.edu>
2019-03-20 20:37:17 -0500

    updated build.

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c8637c104b7baef29ccd347348a2e73b.js"
+  "/react-avengers-gallery/precache-manifest.49079b486a1bd9fa0a6a257f17bee46f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-avengers-gallery/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
