837a3b9b3506c48035e539769cc0793fefedfa37
Billy Shelton <sheltonbilly@utexas.edu>
2019-03-20 20:18:11 -0500

    ran npm run build to prepare the react application.

---
+++ build/precache-manifest.c8637c104b7baef29ccd347348a2e73b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc75a56eec42f2f3c6ea",
+    "url": "/static/css/main.edce91df.chunk.css"
+  },
+  {
+    "revision": "fc75a56eec42f2f3c6ea",
+    "url": "/static/js/main.4f5fdc9d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a4220da4a75c3345bd38",
+    "url": "/static/js/2.705836fd.chunk.js"
+  },
+  {
+    "revision": "70e991df5991c5a529129e315625d904",
+    "url": "/index.html"
+  }
+];
